de1fdd8c62463738f37b4ebaf96e1229fc509950
Evanfeenstra <evanfeenstra@gmail.com>
2025-04-22 20:02:36 -0700

    test pr

--- ast/src/testing/react/mod.rs
+++ ast/src/testing/react/mod.rs
@@ -19,7 +19,7 @@ pub async fn test_react_typescript_generic<G: Graph>() -> Result<(), anyhow::Err
 
     let (num_nodes, num_edges) = graph.get_graph_size();
     if use_lsp == true {
-        assert_eq!(num_nodes, 56, "Expected 56 nodes");
+        assert_eq!(num_nodes, 69, "Expected 56 nodes");
         assert_eq!(num_edges, 77, "Expected 77 edges");
     } else {
         assert_eq!(num_nodes, 50, "Expected 50 nodes");
